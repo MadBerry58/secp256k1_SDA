40d5898adab1e79dcffde4c1fd5bc2523c455720
MMax95 <maxim.mura95@e-uvt.ro>
2022-04-19 23:23:36 +0300

    (Probably) working version

---
+++ build/CMakeFiles/secp256k1_SDA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secp256k1_SDA.dir/main.cpp.o"
+  "CMakeFiles/secp256k1_SDA.dir/src/Mod.cpp.o"
+  "CMakeFiles/secp256k1_SDA.dir/src/Operators.cpp.o"
+  "CMakeFiles/secp256k1_SDA.dir/src/Point.cpp.o"
+  "secp256k1_SDA"
+  "secp256k1_SDA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secp256k1_SDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/muram/OneDrive/Desktop/Facultate/PCD/secp256k1_SDA
+# Build directory: /mnt/c/Users/muram/OneDrive/Desktop/Facultate/PCD/secp256k1_SDA/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
